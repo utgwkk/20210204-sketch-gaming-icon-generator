80fd7df6581635190bafe430be0d3bb736e52642
utgwkk <utagawakiki@gmail.com>
2021-02-05 00:29:58 +0900

    compose them!!!!!!!

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Image } from "./components/Image";
+import { Effecter } from "./components/Effecter";
 
 function App() {
+  const [hueRotate, setHueRotate] = useState(0);
+  const [contrast, setContrast] = useState(100);
+  const [src, setSrc] = useState("https://utgw.net/img/nikutai.png");
+
   return (
     <div className="App">
       <h1>It works!</h1>
+      <Image src={src} hueRotate={hueRotate} contrast={contrast} />
+      <input
+        type="url"
+        size={100}
+        value={src}
+        onChange={(e) => setSrc(e.target.value)}
+      />
+      <Effecter value={hueRotate} min={0} max={360} setValue={setHueRotate} />
+      <Effecter value={contrast} min={0} max={1000} setValue={setContrast} />
     </div>
   );
 }
